97dd61fe01e0147b41304d24b72d82b1d08b1371
Ciara Bujanos <c.buj22@gmail.com>
2017-08-20 22:12:01 -0600

    final

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
-const express = require('express');  //requires the Express module that was installed via NPM
-const app = express(); //sets up the Express application
-const path = require('path'); //part of letting you access the absolute path
-const bodyParser = require('body-parser'); //need for parsing the body of posts
-const shortHash = require('short-hash'); //npm package for url shortener
-const validUrl = require('valid-url'); //npm package for validating urls
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const shortHash = require('short-hash');
+const validUrl = require('valid-url');
 
-const environment = process.env.NODE_ENV || 'development' //sets the environment to dev or product etc..
-const configuration = require('../knexfile')[environment] //for the test environment
-const database = require('knex')(configuration) //runs knex function and passes it the test environment
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
 
-app.set('port', process.env.PORT || 3000); //dynamic port, either localhost or for your heroku address
-app.use(bodyParser.json()); //for every request run body parser so it can read the body on posts
-app.use(bodyParser.urlencoded({ extended: true })) //parses full objects
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use() says for every request to the server, always run the function passed into app.use()
-//In this case we’re saying, for every request to the server, make sure to use the specified path as a starting point for all static asset files.
-//path.join(__dirname, 'public') part is the absolute path of the public directory, and __dirname is a variable that holds the directory of the current Node application
 
 app.get('/', (request, response) => {
 
 });
-//the home endpoint where everything is rendered
 
 app.get('/api/v1/folders', (request, response) => {
   database('folders').select()
@@ -31,7 +27,6 @@ app.get('/api/v1/folders', (request, response) => {
       response.status(500).json({ error })
     });
 });
-//get request routing to folders endpoint, pass folders table to database, give it response status 200 if success or 500 if error
 
 app.get('/api/v1/links', (request, response) => {
   database('links').select()
@@ -42,8 +37,6 @@ app.get('/api/v1/links', (request, response) => {
       response.status(500).json({ error })
     });
 });
-
-//get request routing to links endpoint, pass links table to database, give it response status 200 if success or 500 if error
 
 app.post('/api/v1/folders', (request, response) => {
   const newFolder = request.body;
@@ -56,8 +49,6 @@ app.post('/api/v1/folders', (request, response) => {
     };
   };
 
-  //post request routing to folders endpoint, enter in required properties, return error if anything required is missing.
-
   database('folders').insert(newFolder, 'id')
     .then(folder => {
       response.status(201).json({ id: folder[0] })
@@ -66,8 +57,6 @@ app.post('/api/v1/folders', (request, response) => {
       response.status(500).json({ error })
     });
 });
-
-//pass folders table to database, pass in the new folder and id. give it response status 201 and id if success, or 500 if error
 
 app.delete('/api/v1/folders/:id', (request, response) => {
   const id = request.params.id
@@ -78,8 +67,6 @@ app.delete('/api/v1/folders/:id', (request, response) => {
   })
 })
 
-//delete request to folders endpoint, pass db the folders table and id, run delete method. if success give status of 200, or 501 if error
-
 app.post('/api/v1/links', (request, response) => {
   const newLink = request.body
   newLink.shortURL = shortHash(newLink.origURL)
@@ -87,7 +74,6 @@ app.post('/api/v1/links', (request, response) => {
   if(!validUrl.isWebUri(newLink.origURL)) {
     return response.status(422).json({ error: `Please enter a valid url`})
   }
-  //post request routing to links endpoint, enter in required properties, return error if anything required is missing.
 
   for(let requiredParameter of ['origURL', 'description', 'folder_id']) {
     if(!newLink[requiredParameter]) {
@@ -96,8 +82,6 @@ app.post('/api/v1/links', (request, response) => {
       })
     }
   }
-
-  //setting more required parameters of description, url and folderid
 
   database('links').insert(newLink, '*')
     .then(link => {
@@ -108,16 +92,12 @@ app.post('/api/v1/links', (request, response) => {
     })
 })
 
-//pass links table to db and insert new link, if success 201 status, 500 status if error
-
 app.get('/api/v1/folders/:id/links', (request, response) => {
   const id = request.params.id
   database('links').where('folder_id', id).select()
   .then((links) => response.status(200).json(links))
   .catch(error => response.status(404).json(error))
 })
-
-//get request to links in a certain folder endpoint, pass links table to db and connect with the foreign key. return appropriate status depending on success or error.
 
 app.route('/api/v1/links/:id')
 .get((request, response) => {
@@ -126,11 +106,8 @@ app.route('/api/v1/links/:id')
   .catch(error => response.status(404).json(error))
 })
 
-//routes to link endpoint, passes links table to db, returns 302 if success then redirects to link at index 0 with original url tacked on
-
 app.listen(app.get('port'), () => {
   console.log('Express running');
 });
-//tells the server to start listening for connections on port localhost 3000
 
 module.exports = app
